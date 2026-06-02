a7c495654e3cccf5360ea9af678367aa655747df
EliasChristmas <eliaschristmas8@gmail.com>
2026-06-02 13:51:24 +0200

    2 uploadzones

--- hooks/generator/models/patrick.ts
+++
@@ -1,9 +0,0 @@
-import type { Model } from '@/types/types';
-
-export const patrickModel: Model = {
-	id: 2,
-	name: 'Patrick',
-	profilePicture: '/Patrick/patrick.png',
-	gender: 'male',
-	selected: false,
-};
